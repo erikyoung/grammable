a47c1bc0927fb17ff5d9eeb73f1e169157cf6d70
ErikYoung <erik.young@rmit.edu.vn>
2017-01-28 00:41:37 +0700

    added updates and edits to spec

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -7,10 +7,9 @@ before_action :authenticate_user!, only: [:new, :create]
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render text: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
+  
 
   def index
   end
@@ -24,9 +23,33 @@ before_action :authenticate_user!, only: [:new, :create]
   end
 end
 
+def edit
+  @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank? 
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
+
+  def render_not_found
+    render text: 'Not Found :(', status: :not_found
+  end
+
+
 end
