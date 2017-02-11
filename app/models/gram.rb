47909f53124108fd5d832064a86f5bafd55886c5
ErikYoung <erik.young@rmit.edu.vn>
2017-02-11 17:27:24 +0700

    deleted most of upload functionality

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,10 +2,9 @@ class Gram < ActiveRecord::Base
   validates :message, presence: true
   validates :picture, presence: true
   has_many :comments
-  has_many :uploads
+
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  mount_uploader :attachment, AttachmentUploader
   
 end
 
